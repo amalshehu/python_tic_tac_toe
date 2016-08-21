ba5913b635aa0ab2eeecc2f642dd43972653e128
Amal Shehu <amalshehu@gmail.com>
2016-08-21 22:50:42 +0530

    Global constants created

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,2 +1,9 @@
 # Tic-Tac-Toe
-# Plays the game of tic-tac-toe against a human opponent
+# Plays the game of tic-tac-toe against a human.
+
+# Global Constants
+slot_1 = "X"
+slot_2 = "O"
+empty_slot = " "
+tie = "TIE"
+num_box= 9
